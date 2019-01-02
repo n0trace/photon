5d947d73267af731f5aaa3b1165fea523807a4c8
n0trace <n0trace@protonmail.com>
2019-01-02 10:23:32 +0800

    [mod] .travis more env

--- middleware/useragent.go
+++ middleware/useragent.go
@@ -73,6 +73,9 @@ func RandomUserAgent(browsers ...string) photon.MiddlewareFunc {
 
 func cacheUserAgent(url string) (err error) {
 	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
